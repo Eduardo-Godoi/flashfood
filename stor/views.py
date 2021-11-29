c993a3d1ed3233f7eafbd80ae40d88ec35b51020
Eduardo <egr.contato@gmail.com>
2021-11-28 22:51:25 -0400

    feat: adding stor and stor_category tables, add view stor and stor/products

--- stor/views.py
+++ stor/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Stor, StorCategory
+from .serializers import StorSerializer
+
+
+class StorView(ModelViewSet):
+    queryset = Stor.objects.all()
+    serializer_class = StorSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+
+    def get_serializer(self, *args, **kwargs):
+        street = self.request.data.get('street', None)
+        district = self.request.data.get('district', None)
+        number = self.request.data.get('number', None)
+        category = self.request.data.get('category')
+
+        self.request.data['category'] = {'name': category}
+
+        self.request.data['adress'] = {
+            'street': street,
+            'district': district,
+            'number': number
+        }
+
+        return super().get_serializer(*args, **kwargs)
+
+    def filter_queryset(self, queryset):
+        if 'category' in self.request.GET:
+            return queryset.filter(name__contains=self.request.GET['category'])
+        return queryset
+
+    @action(detail=True, methods=['get'], url_path='products')
+    def list_product(self, request,  *args, **kwargs):
+        if 'category' in self.request.GET:
+            stor = get_object_or_404(Stor, id=kwargs.get('pk'))
+            return stor.products.filter(name__contains=self.request.GET['category'])
+        serialized = ProductSerializer(stor.products, many=True)
+        return Response(serialized.data)
