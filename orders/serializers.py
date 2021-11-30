0d59c8ab76de7c392da2871f21418a77e5fcb288
Charles Pinheiro <charles_fagundes@hotmail.com>
2021-11-30 15:59:17 -0300

    feat: orders views

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from orders.models import Order, orders_products
 from products.serializers import ProductSerializer
 from accounts.serializers import UserSerializer
+from products.models import Product
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -12,6 +13,19 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'product', 'user', 'date', 'total_price']
         depth = 1
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        products_data = validated_data.pop('product')
+        order = Order.objects.create(**validated_data)
+
+        for product_data in products_data:
+            product, _ = Product.objects.get_or_create(name=product_data['name'])
+            order.product.add(product)
+
+        order.user.add(user_data)
+
+        return order
 
 
 class OrderProduct(serializers.ModelSerializer):
