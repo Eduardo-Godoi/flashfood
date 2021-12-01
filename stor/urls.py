3b263985fb7012f76a20c48015c3f3d53f4c02b1
Eduardo <egr.contato@gmail.com>
2021-12-01 15:31:52 -0400

    feat: added the feature to list stores close to the customer

--- stor/urls.py
+++ stor/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import StorView
+from .views import StorView, ShowNearbyStores
 
 router = SimpleRouter()
 router.register(prefix=r'stor', viewset=StorView)
+router.register(prefix=r'category', viewset=ShowNearbyStores)
 
 urlpatterns = router.urls
 
