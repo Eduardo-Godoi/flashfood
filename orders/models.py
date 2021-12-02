ae258c6d12431e7fdec2299136c670b99d751f36
Humberto <matheushumberto520@gmail.com>
2021-12-02 16:05:21 -0400

    Fix: fix orders and refact

--- orders/models.py
+++ orders/models.py
@@ -3,13 +3,14 @@ from django.db import models
 class Order(models.Model):
 
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='orders')
+    products = models.ManyToManyField('products.Product', through='orders.OrderProduct')
     date = models.DateTimeField()
-    total_price = models.FloatField()
+    total_price = models.FloatField(default=0)
 
 
-class orders_products(models.Model):
+class OrderProduct(models.Model):
 
     product = models.ForeignKey('products.Product', on_delete=models.PROTECT)
-    order = models.ForeignKey('orders.Order', on_delete=models.PROTECT)
+    order = models.ForeignKey('orders.Order', on_delete=models.PROTECT, related_name='order_products')
     unit_price = models.FloatField()
     quantity = models.IntegerField()
--- orders/views.py
+++ orders/views.py
@@ -1,30 +1,50 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from rest_framework import mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+from stor.models import Stor
+from utils.permissions import IsCustumerPermission
+
 from orders.models import Order
 from orders.serializers import OrderSerializer
 
 
-class OrderView(GenericAPIView):
+class OrderRequestView(CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsCustumerPermission]
+    
+    def create(self, request, *args, **kwargs):
+        stor = get_object_or_404(Stor, id=kwargs.get('pk'))
+        
+        request.data['stor'] = stor.id
+        return super().create(request, *args, **kwargs)
+    
+class OrderListRetrieveView(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def filter_queryset(self, queryset):
+        user = self.request.user
+        if not user.is_superuser:
+            queryset = queryset.filter(user=self.request.user)
+        
+        return super().filter_queryset(queryset)
+    
+    def get_object(self):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        
+        object = get_object_or_404(Order, **filter_kwargs)
 
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-    def get(self, request):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-
-    def retrieve(self, request, *args, **kwargs):
-        object = self.get_object()
-        serializer = self.get_serializer(object)
-        return Response(serializer.data)
+        if object.user.id != self.request.user.id:
+            raise PermissionDenied("You don't have permission to access this order")
+        
+        return object
+    
